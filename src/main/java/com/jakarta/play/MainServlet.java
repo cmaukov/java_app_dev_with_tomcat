a57d7b1387a4b89b1019db2ba45e01c21e66f9d2
konstantinstaykov <k.staykov@gmail.com>
2022-06-01 22:14:19 -0400

    Initializing Servlets

--- src/main/java/com/jakarta/play/MainServlet.java
+++ src/main/java/com/jakarta/play/MainServlet.java
@@ -5,20 +5,32 @@ package com.jakarta.play;
  */
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/home", "*.do", ""})
+//@WebServlet(urlPatterns = {"/home", "*.do", ""},initParams = {@WebInitParam(name = "productName",value = "My Super Product")})
+@WebServlet(urlPatterns = {"/home", "*.do", ""}, name = "Main")
 public class MainServlet extends HttpServlet {
+    String product = "My Product";
+
+    @Override
+    public void init() throws ServletException {
+       product = getInitParameter("productName");
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/xml");
         String name = req.getParameter("name");
         if (name != null) {
-            resp.getWriter().println(String.format("<name>Hello %s</name>", name));
+            resp.getWriter().println(String.format("<application>" +
+                    "<name>Hello, %s</name>"+
+                    "<product>%s</product>"+
+                    "</application>", name, product));
         }else {
             resp.getWriter().println(String.format("<msg>Please enter your name</msg>"));
         }
