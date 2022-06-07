44d4d7afc897234ef14bb513c6263eb8c8167692
konstantinstaykov <k.staykov@gmail.com>
2022-06-06 22:27:55 -0400

    Logging Filter

---
+++ src/main/java/com/jakarta/play/filters/LoggingFilter.java
@@ -0,0 +1,27 @@
+package com.jakarta.play.filters;
+/* demo
+ * @created 06/06/2022
+ * @author Konstantin Staykov
+ */
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+@WebFilter("/*")
+public class LoggingFilter implements Filter {
+
+    static Logger logger = LogManager.getLogger(LoggingFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        logger.debug(String.format("Request made to %s", req.getRequestURI()));
+        chain.doFilter(request, response);
+    }
+
+
+}
